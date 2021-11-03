979edde8dcf71a4e0635908b442d21bac24bc08a
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-03 11:13:44 +0700

    translation: change string

--- translation.py
+++ translation.py
@@ -1,16 +1,30 @@
 class Translation(object):
-    START_TEXT = """Hai!
-Kegunaan Bot ini untuk membantu anda mengambil API ID & API HASH Telegram Dengan mudah.
-Silahkan Masukkan Nomor Telepon Telegram Anda Dengan Format +62(nohp).
+    START_TEXT = """<b>𝙎𝘾𝙍𝘼𝙋 𝙈𝘼𝙉 di buat untuk Membantu anda Untuk Mengambil APP ID dan API Hash dengan Mudah dan AMAN!
+━━━━━━━━━━━━━━━━━━━━━━━━
+Silahkan Masukkan Nomor Telepon Telegram Anda Dengan Format Kode Negara.
+Contoh : +628xxxxxxx</b>
 """
     AFTER_RECVD_CODE_TEXT = """No HP Diterima!
-Sekarang silakan kirim kode Telegram yang Anda terima dari Telegram!
+Silahkan kirimkan kode yang Anda terima dari Telegram!
 
 Kode ini hanya digunakan untuk tujuan mendapatkan ID APP dari my.telegram.org
-jika Anda tidak mempercayai dev bot ini, Ngambil Manual aja Goblokk 😁🙏
+jika Anda tidak mempercayai dev bot ini, Ngambil Manual aja
 """
     BEFORE_SUCC_LOGIN = "Kode Diterima. Scarpping Web Page..."
-    ERRED_PAGE = "Hadeh Error. Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @mrismanaziz"
+    ERRED_PAGE = """Hadeh Error. Coba dengan Cara Manual
+
+Cara Ambil APP ID dan API HASH Secara Manual
+1. Buka my.telegram.org/auth
+2. Loginkan akun telegram kalian
+3. klik menu API Development
+4. isi data seperti dibawah ini :
+• App Title : tgbot
+• Short Name : tgbot
+• URL : (kosongin)
+• Platform : desktop
+5. Selesai
+
+Bila Berhasil Ambil Manual Silahkan Coba Lagi di Bot ini"""
     CANCELLED_MESG = "Bye! Silahkan /start kembali untuk mengulang"
-    IN_VALID_CODE_PVDED = "Masukan Kode Telegram yang Bener Goblok 😁🙏"
-    IN_VALID_PHNO_PVDED = "Masukan NO HP yang Bener Goblok 😁🙏 Pake Format +(kode Negara) Contoh +62(nohp)"
+    IN_VALID_CODE_PVDED = "Kode OTP yang anda Masukan SALAH"
+    IN_VALID_PHNO_PVDED = "No HP yang anda masukan SALAH, Silahkan Masukkan Nomor Telepon Telegram Anda Dengan Format Kode Negara.\nContoh: +628xxxxxxx"
